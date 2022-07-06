60d25a64b136835af470a44e7b5da411c44cf74d
Sejal kumari <108515140+sejalgit@users.noreply.github.com>
2022-07-06 10:49:49 +0530

    Add files via upload

---
+++ total, grade.cpp
@@ -0,0 +1,12 @@
+# include<stdio.h>
+# include<conio.h>
+int main()
+{
+	int a,b,c,sum;
+	printf("enter the numbers\n ");
+	scanf("%d%d%d",&sum);
+	sum=a+b+c;
+	printf("%d%d%d",&sum );
+	
+	return 0;
+}
